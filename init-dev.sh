195e57c0f9f21cdc37917abed287dc2109018f6d
BartGo <bartoszgo@gmail.com>
2015-05-25 00:18:50 +0200

    ...

---
+++ init-dev.sh
@@ -0,0 +1,19 @@
+
+MY_VENV="venv-console-spacewar"
+
+echo "Upgrade tools..."
+pip install --user --upgrade pip
+pip install --user --upgrade virtualenv
+pip install --user --upgrade vex
+echo "Purge and recreate virtual environment named $MY_VENV..."
+vex -r $MY_VENV pip --version
+vex -m $MY_VENV python --version
+echo "Download requirements and keep downloaded packages..."
+vex $MY_VENV pip install -r requirements.txt
+
+echo "cd source & vex $MY_VENV python spacewar.py & cd .." > run-dev.sh
+echo "vex $MY_VENV" > sh-dev.sh
+echo ""
+echo "To run in venv $MY_VENV: run-dev.sh"
+echo "Shell, in venv $MY_VENV: sh-dev.sh"
+echo ""
